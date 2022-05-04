20645fee6a1fe57571fcf3c1c8f749591f277e75
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2022-05-04 17:31:14 +0100

    Update _config.ts

--- _config.ts
+++ _config.ts
@@ -1,13 +1,12 @@
-import lume from 'lume/mod.ts'
-import sass from 'lume/plugins/sass.ts'
-import terser from 'lume/plugins/terser.ts'
-import relative_urls from 'lume/plugins/relative_urls.ts'
-import pug from 'lume/plugins/pug.ts'
-import jsx from 'lume/plugins/jsx.ts'
-import postcss from 'lume/plugins/postcss.ts'
+import lume from '/lume/mod.ts'
+import sass from '/lume/plugins/sass.ts'
+import terser from '/lume/plugins/terser.ts'
+import relative_urls from '/lume/plugins/relative_urls.ts'
+import pug from '/lume/plugins/pug.ts'
+import jsx from '/lume/plugins/jsx.ts'
+import postcss from '/lume/plugins/postcss.ts'
 
 const site = lume({
-  location: new URL('https://example.com/'),
   src: './src',
 })
 site.copy('static')
