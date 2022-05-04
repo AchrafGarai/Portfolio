a963e5df26bed8b5080e520695335db999d22275
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2022-05-04 18:05:57 +0100

    Update _config.ts

--- _config.ts
+++ _config.ts
@@ -2,7 +2,6 @@ import lume from '/lume/mod.ts'
 import sass from '/lume/plugins/sass.ts'
 import terser from '/lume/plugins/terser.ts'
 import relative_urls from '/lume/plugins/relative_urls.ts'
-import pug from '/lume/plugins/pug.ts'
 import jsx from '/lume/plugins/jsx.ts'
 
 
