d69cb255f2057086d4cb37f052cf56c9d933a981
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2022-05-04 19:17:39 +0100

    update config

--- _config.ts
+++ _config.ts
@@ -4,7 +4,7 @@ import sass from 'https://deno.land/x/lume@v1.7.4/plugins/sass.ts'
 import terser from 'https://deno.land/x/lume@v1.7.4/plugins/terser.ts'
 import relative_urls from 'https://deno.land/x/lume@v1.7.4/plugins/relative_urls.ts'
 import postcss from 'https://deno.land/x/lume@v1.7.4/plugins/postcss.ts'
-
+import basePath from "https://deno.land/x/lume@v1.7.4/plugins/base_path.ts";
 const site = lume({
   src: './src',
    location: new URL("https://achrafgarai.github.io/Portfolio/"),
@@ -14,5 +14,6 @@ site.use(relative_urls())
 site.use(sass({ extensions: ['.scss'] }))
 site.use(postcss())
 site.use(terser())
+site.use(basePath());
 
 export default site
